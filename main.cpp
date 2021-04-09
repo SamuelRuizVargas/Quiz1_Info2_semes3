7ace86623bba5ed27029137236720469c28944ff
SamuelRuizVargas <samuel.ruizv@udea.edu.co>
2021-04-09 12:40:47 -0500

    Quiz terminado

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Ingrese un numero entero: ";cin>>n;
+    int n_co=n, suma=0, cifra,potencia, cifra_E=0, conta_E=0, suma_po;;
+    while(n_co>0)
+    {
+        cifra=n_co%10;
+        if(cifra>cifra_E)
+        {
+            cifra_E=cifra;
+            conta_E=1;
+        }
+        else if(cifra==cifra_E)
+        {
+            conta_E++;
+        }
+        potencia=cifra;
+        for (int i=1;i<cifra;i++)
+        {
+            suma_po=0;
+            for(int j=0;j<potencia;j++)
+            {
+                suma_po+=cifra;
+            }
+            potencia=suma_po;
+        }
+        suma+=potencia;
+        n_co=n_co/10;
+    }
+    cout<<"La suma de cada uno de los digitos elevados a si mismos del numero "<<n<<" es "<<suma<<endl;
+    cout<<"El digito mayor encontrado es el "<<cifra_E<<" que aparece "<<conta_E<<" vez(ces)"<<endl;
+}
